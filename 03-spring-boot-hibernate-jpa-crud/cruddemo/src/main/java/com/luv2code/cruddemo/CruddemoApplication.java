f6c6505d30e6805e74e6a7504b917d71cdcbd323
Prakhar Sankrityayan <prakhar3099@gmail.com>
2023-05-02 03:17:32 +0530

    Saving a Java Object with JPA

--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.StudentDAO;
+import com.luv2code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,20 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello world");
+			createStudent(studentDAO);
 		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 	}
 
 }
--- 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/entity/Student.java
+++ 03-spring-boot-hibernate-jpa-crud/cruddemo/src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -11,18 +11,18 @@ public class Student {
     private int id;
 
     @Column(name="first_name")
-    private int firstName;
+    private String firstName;
 
     @Column(name="last_name")
-    private int lastName;
+    private String lastName;
 
     @Column(name="email")
-    private int email;
+    private String email;
 
     public Student() {
 
     }
-    public Student(int firstName, int lastName, int email) {
+    public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -36,27 +36,27 @@ public class Student {
         this.id = id;
     }
 
-    public int getFirstName() {
+    public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(int firstName) {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public int getLastName() {
+    public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(int lastName) {
+    public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public int getEmail() {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(int email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 
