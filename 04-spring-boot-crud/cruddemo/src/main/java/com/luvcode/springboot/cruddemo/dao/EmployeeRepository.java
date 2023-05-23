74d2b67f41acf5b8ed65a870bf06c848230b305f
Prakhar Sankrityayan <prakhar3099@gmail.com>
2023-05-24 03:34:11 +0530

    Spring Boot REST API Security - Basic Configuration

--- 04-spring-boot-crud/cruddemo/src/main/java/com/luvcode/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-crud/cruddemo/src/main/java/com/luvcode/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,10 @@ package com.luvcode.springboot.cruddemo.dao;
 
 import com.luvcode.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.stereotype.Repository;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
